83a5abf92af9b2b6815f5aa2619b998d22377fa2
Ikram M. Mirza <mimurtaza@hotmail.com>
2021-03-26 22:57:30 +0000

    More work on HTML template

--- app.py
+++ app.py
@@ -1,14 +1,18 @@
-from flask import Flask
+from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return "<h1>Hello World!</h1>"
+@app.route("/<name>")
+def home(name):
+    return render_template("index.html", content=name, r=2)
 
-@app.route('/testname')
-def testname():
-    return "<h1>Hello World, Test!</h1>"
+@app.route("/<sname>")
+def user(sname):
+    return f"<h1>Hello World, {sname}!</h1>"
+
+@app.route("/admin")
+def admin():
+    return redirect(url_for("user", sname="Admin"))
 
 if __name__ == '__main__':
     app.run(debug=True)
