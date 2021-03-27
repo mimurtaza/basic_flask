29833c458a7f0feb202f3e081aa25c828cb97ea1
Ikram M. Mirza <mimurtaza@hotmail.com>
2021-03-27 22:15:49 +0000

    Extending base html

--- app.py
+++ app.py
@@ -2,17 +2,9 @@ from flask import Flask, redirect, url_for, render_template
 
 app = Flask(__name__)
 
-@app.route("/<name>")
-def home(name):
-    return render_template("index.html", content=name, r=2)
-
-@app.route("/<sname>")
-def user(sname):
-    return f"<h1>Hello World, {sname}!</h1>"
-
-@app.route("/admin")
-def admin():
-    return redirect(url_for("user", sname="Admin"))
+@app.route("/")
+def home():
+    return render_template("index.html")
 
 if __name__ == '__main__':
     app.run(debug=True)
